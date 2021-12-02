1829faac7401afb4f4b9df4f38eb59cc74947255
modifiedAlex <75803920+GeneticallyModifiedAlex@users.noreply.github.com>
2021-12-02 20:32:51 +0000

    finalised the navbar layout

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -1,8 +1,35 @@
 document.write(`
 <div class="overlay-content">
 
-<a class="Title" href="#">SERVICES</a>
-<a href="#">PROJECTS</a>
-<a href="#">NEWS</a>
-<a href="#">About US</a>
+    <div class="Nav-Card">
+        <a class="Title" href="#"><h1>SERVICES</h1></a>
+        <div class="Description">
+            <h5 class="line">Looking to rent a server?</h5>
+            <h5 class="line">Or perhaps wanting to own a website like ours?</h5>
+        </div>
+        <div class="seperator"></div>
+    </div>
+    <div class="Nav-Card">
+        <a class="Title" href="#"><h1>PROJECTS</h1></a>
+        <div class="Description">
+            <h5 class="line">A look into projects we have been involved in</h5>
+            <h5 class="line">including the free products we released on GitHub</h5>
+        </div>
+        <div class="seperator"></div>
+    </div>
+    <div class="Nav-Card">
+        <a class="Title" href="#"><h1>NEWS</h1></a>
+        <div class="Description">
+            <h5 class="line">News we personally think are relevant to be shared from us for all of you</h5>
+        </div>
+        <div class="seperator"></div>
+    </div>
+    <div class="Nav-Card">
+        <a class="Title" href="#"><h1>ABOUT US</h1></a>
+        <div class="Description">
+            <h5 class="line">Take a look at our team</h5>
+            <h5 class="line">Find out our contact details for further enquiry</h5>
+        </div>
+        <div class="seperator"></div>
+    </div>
 </div>`)
