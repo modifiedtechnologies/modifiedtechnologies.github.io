74270e07d6a20cebc48441129dd0fd3797b70b8e
modifiedAlex <75803920+GeneticallyModifiedAlex@users.noreply.github.com>
2021-12-02 19:24:54 +0000

    READ: making it multiline

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -1,1 +1,7 @@
-document.write('<div class="overlay-content"><a href="#">SERVICES</a><a href="#">PROJECTS</a><a href="#">NEWS</a><a href="#">About US</a></div>')
+document.write(`
+<div class="overlay-content">
+<a href="#">SERVICES</a>
+<a href="#">PROJECTS</a>
+<a href="#">NEWS</a>
+<a href="#">About US</a>
+</div>`)
