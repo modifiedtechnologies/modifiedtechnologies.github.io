f965c81552294ff46cbad1994705a9d4617fc8bd
modifiedAlex <75803920+GeneticallyModifiedAlex@users.noreply.github.com>
2021-12-02 23:12:23 +0000

    js change. overlay color comment

--- Scripts/nav_overlay.js
+++ Scripts/nav_overlay.js
@@ -1,9 +1,11 @@
 /* Open */
 function openNav() {
+    document.getElementById("overlaybtn").style.display="none";
     document.getElementById("myNav").style.height = "100%";
 }
   
   /* Close */
 function closeNav() {
+    document.getElementById("overlaybtn").style.display="block";
     document.getElementById("myNav").style.height = "0%";
 } 
