ba1c22e5206c8e55c2b4ba32fd6b016f586c7b0d
modifiedAlex <75803920+GeneticallyModifiedAlex@users.noreply.github.com>
2021-12-03 20:52:38 +0000

    start

--- Scripts/nav_overlay.js
+++ Scripts/nav_overlay.js
@@ -1,11 +1,9 @@
 /* Open */
 function openNav() {
-    document.getElementById("overlaybtn").style.display="none";
     document.getElementById("myNav").style.height = "100%";
 }
   
   /* Close */
 function closeNav() {
-    document.getElementById("overlaybtn").style.display="block";
     document.getElementById("myNav").style.height = "0%";
 } 
