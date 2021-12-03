489acda66a852f6988896ccf72840580ef059921
modifiedAlex <75803920+GeneticallyModifiedAlex@users.noreply.github.com>
2021-12-03 21:50:16 +0000

    FEAT: generic navbar loader

---
+++ Scripts/navbar/navbar.js
@@ -0,0 +1,10 @@
+//loads in the default navbar for each page. Reusable
+
+document.write(`
+    <div id="navbar">
+        <img id="logo" src="./assets/images/Logo.svg" alt="" href="#" srcset="">
+        <div id="overlaybtn"  onclick="openNav()">
+            <img class="NavButton"src="./assets/images/Menu.svg" alt="" srcset="">
+        </div>
+    </div>
+`)
