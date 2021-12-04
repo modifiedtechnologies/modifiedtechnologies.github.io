43972c0c137e2bc29fae26f960f0c3c0f9fb1e42
modifiedAlex <75803920+GeneticallyModifiedAlex@users.noreply.github.com>
2021-12-04 16:11:44 +0000

    socials added

---
+++ Scripts/socials.js
@@ -0,0 +1,24 @@
+
+
+
+function socials(){
+    document.write(`
+        <div class="socials">
+            <div class="social-icons">
+                <a class="icon" href="http://">
+                    <img src="../assets/images/Social-Icons/LinkedIn.svg" alt="" srcset="">
+                </a>
+                <div class="social-seperator"></div>
+            </div>
+
+            <div class="social-icons">
+                <a class="icon" href="http://">
+                    <img class="icon"src="../assets/images/Social-Icons/github.svg" alt="" srcset="">
+                </a>
+                <div class="social-seperator"></div>
+            
+            </div>
+
+        </div>
+    `);
+}
