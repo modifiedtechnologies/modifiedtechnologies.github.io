ff24192bbee2a29f109477b4945065f18ddc940d
modifiedAlex <75803920+GeneticallyModifiedAlex@users.noreply.github.com>
2021-12-04 16:19:15 +0000

    fixing paths

--- Scripts/socials.js
+++ Scripts/socials.js
@@ -6,14 +6,14 @@ function socials(){
         <div class="socials">
             <div class="social-icons">
                 <a class="icon" href="http://">
-                    <img src="../assets/images/Social-Icons/LinkedIn.svg" alt="" srcset="">
+                    <img src="./assets/images/Social-Icons/LinkedIn.svg" alt="" srcset="">
                 </a>
                 <div class="social-seperator"></div>
             </div>
 
             <div class="social-icons">
                 <a class="icon" href="http://">
-                    <img class="icon"src="../assets/images/Social-Icons/github.svg" alt="" srcset="">
+                    <img class="icon"src="./assets/images/Social-Icons/github.svg" alt="" srcset="">
                 </a>
                 <div class="social-seperator"></div>
             
